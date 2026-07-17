718745f2d189395bde831da1cb1524643500dbc4
denisarwanto190590-code <denisarwanto190590@gmail.com>
2026-07-17 17:20:54 +0700

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,22 +1,18 @@
-//
-// Created by Reveny on 2023/1/13.
-//
-
-#include <zenith.h>
-#include "modmenu.h"
-
-REGISTER_ZYGISK_MODULE(ZenithModule)
-REGISTER_ZYGISK_COMPANION(ZenithCompanion)
-
-void ZenithModule::onFieldsLoaded(JNIEnv *env, jstring appDataDir) {
-    if (isGame(env, appDataDir)) {
-        enable_hack = 1;
+// --- MAIN HACK THREAD ---
+void *hack_thread(void *) {
+    unsigned long il2cpp_base = 0;
+    
+    // Tunggu sampai library Free Fire termuat di memori
+    while (!il2cpp_base) {
+        il2cpp_base = get_module_base("libil2cpp.so");
+        sleep(1);
     }
-}
 
-void ZenithModule::onModuleLoaded() {
-    if (enable_hack) {
-        pthread_t t;
-        pthread_create(&t, nullptr, hack_thread, nullptr);
-    }
+    // [HOOK 1] Mengunci offset get_Position yang Anda temukan sebelumnya
+    DobbyHook((void *)(il2cpp_base + 0x7CEE8A0), (void *)hk_GetPosition, (void **)&old_GetPosition);
+
+    // [HOOK 2] Mengunci offset Kamera UGCAPIWorldToScreenPoint yang baru saja ketemu
+    DobbyHook((void *)(il2cpp_base + 0x80BFB2C), (void *)hk_WorldToScreenPoint, (void **)&old_WorldToScreenPoint);
+
+    return nullptr;
 }
