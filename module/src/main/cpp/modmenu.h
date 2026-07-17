535b86be9f08379c01c8934e39f7368426d364f0
denisarwanto190590-code <denisarwanto190590@gmail.com>
2026-07-17 17:35:12 +0700

    Update modmenu.h

--- module/src/main/cpp/modmenu.h
+++ module/src/main/cpp/modmenu.h
@@ -1,146 +1,103 @@
 //
 // Created by Reveny on 2023/1/13.
-// Updated for Free Fire Max ESP Line (Pure Standard C++)
 //
-#define targetLibName "libil2cpp.so"
+#define targetLibName OBFUSCATE("libil2cpp.so")
 
 #include <jni.h>
 #include <sys/system_properties.h>
-#include <cmath>
-#include <android/log.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 
 #include "KittyMemory/MemoryPatch.h"
 #include "Includes/ESP.h"
 #include "Includes/Dobby/dobby.h"
 #include "Includes/Utils.h"
 #include "Includes/ImGui.h"
+#include "modmenu.h" // Menghubungkan ke file menu yang sudah kita buat
 
-// Makro Log Android Standar agar tidak memicu eror "undefined"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ZygiskMenu", __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, "ZygiskMenu", __VA_ARGS__)
+static int enable_hack;
+static char *game_data_dir = nullptr;
 
-// ====================================================================
-// DAFTAR OFFSET MATANG ESP LINE (FREE FIRE MAX)
-// ====================================================================
-#define OFFSET_GET_MAIN                 0xa7ed6c0
-#define OFFSET_WORLD_TO_SCREEN          0xa7ed344
-#define OFFSET_GET_PLAYER_COUNT         0x645d5c4
-#define OFFSET_GET_LOCAL_PLAYER         0x64cbde8
-#define OFFSET_GET_PLAYER_BY_INDEX      0x7d3fb8c
-#define OFFSET_GET_POSITION             0x8857b00
-#define OFFSET_IS_DEAD                  0x76611dc
+// 1. DIUBAH: Menyesuaikan package name target ke Free Fire sesuai hasil Frida Dumper Anda
+static char *game_package_name = "com.dts.freefireth";
 
-// Struktur data matematika standar untuk koordinat game
+// Struktur data Vector3 untuk koordinat game Unity
 struct Vector3 {
     float x, y, z;
 };
-struct Vector2 {
-    float x, y;
-};
 
-static int enable_hack = 0;
-static char *game_data_dir = nullptr;
-static const char *game_package_name = "com.dts.freefiremax"; 
+// --- LOGIKA HOOK OFFSET ESP ---
 
-// Fungsi pembantu untuk membaca memori RAM (Memory Reading)
-template <typename T>
-T BacaMemori(uintptr_t address) {
-    if (address == 0) return T();
-    return *reinterpret_cast<T*>(address);
+// Pointer fungsi asli get_Position
+void *(*old_GetPosition)(void *instance);
+
+// Fungsi Hook untuk mengambil posisi objek/pemain
+void *hk_GetPosition(void *instance) {
+    if (instance != nullptr) {
+        // Di sini tempat ImGui memproses koordinat dari instance untuk digambar ke layar
+    }
+    return old_GetPosition(instance);
 }
 
-// Fungsi Utama untuk Menggambar ESP Line
-void RenderESPLine(uintptr_t il2cppBase) {
-    uintptr_t mainCameraFunc = BacaMemori<uintptr_t>(il2cppBase + OFFSET_GET_MAIN);
-    if (!mainCameraFunc) return;
+// Pointer fungsi asli UGCAPIWorldToScreenPoint (Kamera)
+void *(*old_WorldToScreenPoint)(void *camera, Vector3 position, int eye, Vector3 *output);
 
-    int totalPemain = BacaMemori<int>(il2cppBase + OFFSET_GET_PLAYER_COUNT);
-    if (totalPemain <= 0 || totalPemain > 100) return; 
+// Fungsi Hook Kamera untuk konversi koordinat 3D ke 2D Layar
+void *hk_WorldToScreenPoint(void *camera, Vector3 position, int eye, Vector3 *output) {
+    return old_WorldToScreenPoint(camera, position, eye, output);
+}
 
-    uintptr_t localPlayer = BacaMemori<uintptr_t>(il2cppBase + OFFSET_GET_LOCAL_PLAYER);
-
-    ImDrawList* drawList = ImGui::GetForegroundDrawList();
-    float layarLebar = ImGui::GetIO().DisplaySize.x;
-    float layarTinggi = ImGui::GetIO().DisplaySize.y;
-    
-    ImVec2 titikAwalGaris = ImVec2(layarLebar / 2.0f, layarTinggi);
-
-    for (int i = 0; i < totalPemain; i++) {
-        uintptr_t playerTarget = BacaMemori<uintptr_t>(il2cppBase + OFFSET_GET_PLAYER_BY_INDEX + (i * sizeof(void*)));
-        if (!playerTarget) continue;
-
-        if (playerTarget == localPlayer) continue;
-
-        bool apakahMati = BacaMemori<bool>(playerTarget + OFFSET_IS_DEAD);
-        if (apakahMati) continue;
-
-        Vector3 posisi3DMusuh = BacaMemori<Vector3>(playerTarget + OFFSET_GET_POSITION);
-
-        Vector2 posisi2DLayar;
-        typedef bool (*t_WorldToScreen)(uintptr_t camera, Vector3 worldPos, Vector2& screenPos);
-        t_WorldToScreen WorldToScreen = (t_WorldToScreen)(il2cppBase + OFFSET_WORLD_TO_SCREEN);
-
-        if (WorldToScreen(mainCameraFunc, posisi3DMusuh, posisi2DLayar)) {
-            ImVec2 titikMusuh = ImVec2(posisi2DLayar.x, posisi2DLayar.y);
-            drawList->AddLine(titkAwalGaris, titikMusuh, IM_COL32(255, 0, 0, 255), 2.0f);
+int isGame(JNIEnv *env, jstring appDataDir) {
+    if (!appDataDir) {
+        return 0;
+    }
+    const char *app_data_dir = env->GetStringUTFChars(appDataDir, nullptr);
+    static char package_name[256];
+    if (sscanf(app_data_dir, OBFUSCATE("/data/%*[^/]/%d/%s"), 0, package_name) != 2) {
+        if (sscanf(app_data_dir, OBFUSCATE("/data/%*[^/]/%s"), package_name) != 1) {
+            package_name[0] = '\0';
+            LOGW(OBFUSCATE("can't parse %s"), app_data_dir);
+            return 0;
         }
     }
-}
-
-// Fungsi pencocokan paket game target 
-bool isGame(JNIEnv *env, jstring appDataDir) {
-    if (!appDataDir) return false;
-    const char *app_data_dir = env->GetStringUTFChars(appDataDir, nullptr);
-    if (!app_data_dir) return false;
-
-    if (strstr(app_data_dir, game_package_name) != nullptr) {
+    if (strcmp(package_name, game_package_name) == 0) {
+        LOGI(OBFUSCATE("detect game: %s"), package_name);
         game_data_dir = new char[strlen(app_data_dir) + 1];
         strcpy(game_data_dir, app_data_dir);
         env->ReleaseStringUTFChars(appDataDir, app_data_dir);
-        return true;
+        return 1;
+    } else {
+        env->ReleaseStringUTFChars(appDataDir, app_data_dir);
+        return 0;
     }
-    env->ReleaseStringUTFChars(appDataDir, app_data_dir);
-    return false;
 }
 
-// Cara murni Linux Android untuk mencari Base Address (SUDAH DIPERBAIKI ARRAY-NYA)
-uintptr_t AmbilBaseAddress(const char* libName) {
-    uintptr_t startAddress = 0;
-    char line[512]; // <--- Sudah diperbaiki menjadi array berukuran 512 karakter
-    FILE* fp = fopen("/proc/self/maps", "rt");
-    if (fp != nullptr) {
-        while (fgets(line, sizeof(line), fp)) {
-            if (strstr(line, libName) != nullptr) {
-                if (sscanf(line, "%lx-", &startAddress) == 1) {
-                    break;
-                }
-            }
-        }
-        fclose(fp);
-    }
-    return startAddress;
-}
-
+// Mengarahkan tampilan menu ke DrawMenu() yang ada di modmenu.h
 void drawMenu() {
-    uintptr_t il2cppBaseAddress = AmbilBaseAddress(targetLibName);
-    if (il2cppBaseAddress != 0) {
-        RenderESPLine(il2cppBaseAddress);
-    }
-    ImGui::ShowDemoWindow();
+    DrawMenu();
 }
 
 void *hack_thread(void *) {
-    LOGI("Hack thread berhasil dijalankan.");
-    
-    uintptr_t il2cppBaseAddress = 0;
-    do {
-        sleep(1);
-        il2cppBaseAddress = AmbilBaseAddress(targetLibName);
-    } while (il2cppBaseAddress == 0);
+    LOGI(OBFUSCATE("hack thread: %d"), gettid());
+    initModMenu((void *)drawMenu);
 
-    LOGI("Library game terdeteksi pada alamat memori!");
+    // Check if target lib is loaded
+    do {
+        LOGI(OBFUSCATE("Trying to find lib... PID: %d"), getpid());
+        sleep(1);
+    } while (!isLibraryLoaded(targetLibName));
+
+    LOGI(OBFUSCATE("%s has been loaded, 0x%lx"), (const char *) targetLibName, getBaseAddress(targetLibName));
+
+    // 2. DIUBAH: Mengambil alamat dasar (Base Address) libil2cpp.so
+    unsigned long il2cpp_base = getBaseAddress(targetLibName);
+
+    // 3. DIUBAH: Memasang DobbyHook menggunakan offset yang Anda temukan
+    // [HOOK 1] Offset get_Position (0x7CEE8A0)
+    dobby_dummy_hook((void *)(il2cpp_base + 0x7CEE8A0), (void *)hk_GetPosition, (void **)&old_GetPosition);
+
+    // [HOOK 2] Offset Kamera UGCAPIWorldToScreenPoint (0x80BFB2C)
+    dobby_dummy_hook((void *)(il2cpp_base + 0x80BFB2C), (void *)hk_WorldToScreenPoint, (void **)&old_WorldToScreenPoint);
+
+    LOGI(OBFUSCATE("Done"));
     return nullptr;
 }
