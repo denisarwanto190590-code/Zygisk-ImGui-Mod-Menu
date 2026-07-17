733fbf7371c8358bb350dc330073bbbfef6b3c1d
denisarwanto190590-code <denisarwanto190590@gmail.com>
2026-07-17 18:28:23 +0700

    Update Android.mk

--- module/src/main/cpp/Android.mk
+++ module/src/main/cpp/Android.mk
@@ -11,16 +11,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := modmenu
 
-# Mendaftarkan folder pencarian header agar compiler bisa membaca zygisk.hpp, dobby.h, dkk
+# Mendaftarkan folder pencarian header
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/Includes \
                     $(LOCAL_PATH)/Includes/Dobby
 
-# Optimasi Kode dan Compiler Flags
+# Optimasi Kode dan Compiler Flags (Sudah ditambahkan perbaikan IMGUI_IMPL_API)
 LOCAL_CFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w
-LOCAL_CFLAGS += -fno-rtti -fno-exceptions -fpermissive
+LOCAL_CFLAGS += -fno-rtti -fno-exceptions -fpermissive -DIMGUI_IMPL_API=""
 LOCAL_CPPFLAGS := -Wno-error=format-security -fvisibility=hidden -ffunction-sections -fdata-sections -w -s -std=c++17
-LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fms-extensions -fno-rtti -fno-exceptions -fpermissive
+LOCAL_CPPFLAGS += -Wno-error=c++11-narrowing -fms-extensions -fno-rtti -fno-exceptions -fpermissive -DIMGUI_IMPL_API=""
 LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all -llog
 LOCAL_ARM_MODE := arm
 
